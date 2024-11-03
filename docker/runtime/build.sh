27a03649de19e5f3390a442d1acc4a952d6b5236
weibaohui <weibaohui@yeah.net>
2024-11-03 19:30:11 +0800

    test(docker): 更新 kind-in-docker 镜像并进行测试运行
    - 构建并推送 weibh/kind-in-docker:docker-27-kind-0.24 镜像- 添加运行测试命令以验证镜像功能

--- docker/runtime/build.sh
+++ docker/runtime/build.sh
@@ -1,2 +1,3 @@
 docker build -t weibh/kind-in-docker:docker-27-kind-0.24 .
 docker push weibh/kind-in-docker:docker-27-kind-0.24
+docker run -it --rm --privileged weibh/kind-in-docker:docker-27-kind-0.24
