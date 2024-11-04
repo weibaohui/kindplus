e3999bea6ec9f9e36ed54350098372a568369d43
weibaohui <weibaohui@yeah.net>
2024-11-04 22:51:45 +0800

    refactor: 添加集群自动发现和初始化应用安装的思路
    - 提出通过自动发现集群中的 kind小集群并自动生成 kubeconfig 的想法
    - 探讨使用公共 PVC 存储各小集群的 kubeconfig 文件的可行性
    -考虑通过 kom 控制小集群，实现从程序中安装集群初始化应用

--- main.go
+++ main.go
@@ -18,4 +18,8 @@ func main() {
 		Runtime: installer.NewRuntime(),
 	}
 	i.Deploy()
+
+	// todo 自动发现集群中的kind小集群，并且自动生成kubeconfig
+	// 是否可以采用一个公共pvc，各个小集群将kubeconfig写入到文件夹中，然后读取，并且注册到kom方便控制？
+	// 如果kom可以控制，那么可以从程序中安装集群的初始化应用
 }
