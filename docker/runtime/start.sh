115709261a11a03efbcb22288e3d2079e31da569
weibaohui <weibaohui@yeah.net>
2024-11-04 16:10:37 +0800

    feat(runtime): 添加 kind 集群部署脚本并配置 kubectl
    - 新增 kind-make.sh脚本用于创建 kind集群
    - 在 Dockerfile 中添加 kubectl 和 kind 支持
    - 更新 build.sh 和 start.sh 以支持新的 kind 部署流程
    - 优化 Dockerfile 以使用多阶段构建

--- docker/runtime/start.sh
+++ docker/runtime/start.sh
@@ -1,3 +1,4 @@
+# !/bin/bash
 #将entrypoint.sh在后台运行
 nohup dockerd-entrypoint.sh > /dev/null 2>&1 &
 # 输出Echo 命令
@@ -10,8 +11,10 @@ check_docker() {
   done
   echo "Docker 已启动。"
 }
-
 # 调用函数检查 Docker 是否启动
 check_docker
+
+# 调用安装kind脚本
+bash kind-make.sh  cluster-2.dev.power.sd.istio.space kind 6552
 
 tail -f /dev/null
