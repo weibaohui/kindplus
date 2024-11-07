530516d899d4ebac906fb779df5ec7ca628692ba
weibaohui <weibaohui@yeah.net>
2024-11-07 16:51:14 +0800

    build(kindplus): 使用 Supervisor管理 KindPlus 进程
    - 在 Dockerfile 中添加 Supervisor 相关的安装和配置- 新增 kindplus.conf 文件用于 Supervisor 配置
    - 修改 reload.sh 脚本，使用 Supervisor 管理进程
    -优化了进程启动和重启逻辑，提高了系统的稳定性和可靠性

--- docker/kindplus/reload.sh
+++ docker/kindplus/reload.sh
@@ -4,25 +4,24 @@ APP_PATH="/app/kindplus"           # 上传的新文件名始终为 kindplus
 CURRENT_PATH="/app/kindplus.current" # 运行中的文件名为 kindplus.current
 WATCH_PATH="/app"                  # 监控的目录
 
+
+
 # 启动 kindplus 程序
 start_app() {
     echo "Starting kindplus..."
     mv $APP_PATH $CURRENT_PATH          # 将 kindplus 重命名为 kindplus.current
     chmod +x $CURRENT_PATH              # 确保文件可执行
-    nohup $CURRENT_PATH > /dev/stdout 2> /dev/stderr &
-    APP_PID=$!
-    echo "kindplus started with PID: $APP_PID"
+    sleep 1
+    supervisord -c /etc/supervisord.conf
+}
+restart_app() {
+    echo "restarting kindplus..."
+    mv $APP_PATH $CURRENT_PATH          # 将 kindplus 重命名为 kindplus.current
+    chmod +x $CURRENT_PATH              # 确保文件可执行
+    sleep 1
+    supervisorctl restart kindplus
 }
 
-# 停止 kindplus 程序
-stop_app() {
-    if [ -n "$APP_PID" ]; then
-        echo "Stopping kindplus with PID: $APP_PID"
-        kill "$APP_PID"
-        wait "$APP_PID" 2>/dev/null
-        echo "kindplus stopped"
-    fi
-}
 
 # 初次启动程序
 start_app
@@ -35,12 +34,8 @@ while true; do
     # 检查是否是新的 kindplus 文件写入完成
     if [ -f "$APP_PATH" ]; then
         echo "Detected new kindplus version, restarting..."
-
-        # 停止当前程序
-        stop_app
-
         # 删除旧的 kindplus.current 文件并启动新的程序
         rm -f $CURRENT_PATH
-        start_app
+        restart_app
     fi
 done
